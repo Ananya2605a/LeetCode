613944dc02e9a887f685d37d5c1a145967688399
Ananya Sharma <ananyashar06@gmail.com>
2026-08-26 23:27:27 +0530

    Time: 2 ms (95.61%), Space: 48.3 MB (60.97%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int left=0;
+        int right=numbers.length-1;
+        while(left<right)
+        {
+            int current = numbers[left]+numbers[right];
+            if(current==target)
+            {
+                return new int[] {left+1,right+1};
+            }
+            else if(current>target)
+            {
+                right--;
+            }
+            else
+            {
+                left++;
+            }
+        }
+        return new int[] {-1,-1};
+        
+    }
+}
