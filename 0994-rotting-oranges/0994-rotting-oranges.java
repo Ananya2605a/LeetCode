bd46923e3cd87d1f9b3665a4d4e9f764005aa3e2
Ananya Sharma <ananyashar06@gmail.com>
2026-08-27 10:33:31 +0530

    Time: 1 ms (100%), Space: 43.9 MB (84.92%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int orangesRotting(int[][] grid) {
+        if(grid==null || grid.length==0)
+        {
+            return 0;
+        }
+        int row = grid.length;
+        int col = grid[0].length;
+        for(int i=0;i<row;i++)
+        {
+            for(int j=0;j<col;j++)
+            {
+                if(grid[i][j]==2)
+                {
+                    dfs(grid,i,j,2,row,col);
+                }
+            }
+        }
+        int maxtime=0;
+        for(int i=0;i<row;i++)
+        {
+            for(int j=0;j<col;j++)
+            {
+                if(grid[i][j]==1) return -1;
+                if(grid[i][j]>2)
+                {
+                    maxtime=Math.max(maxtime,grid[i][j]-2);
+                }
+            }
+        }
+        return maxtime;
+        
+    }
+    public void dfs(int[][] grid,int i,int j, int min , int row,int col)
+    {
+        if(i<0||j<0||i>=row||j>=col||grid[i][j]==0)
+        {
+            return;
+        }
+        if(grid[i][j]>1 && grid[i][j]<min)
+        {
+            return;
+        }
+        grid[i][j]=min;
+        dfs(grid,i+1,j,min+1,row,col);
+        dfs(grid,i-1,j,min+1,row,col);
+        dfs(grid,i,j+1,min+1,row,col);
+        dfs(grid,i,j-1,min+1,row,col);
+    }
+}
