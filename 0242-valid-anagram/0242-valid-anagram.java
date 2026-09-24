c3cca9386d3697935caf10f15bd7fec5c798e81d
Ananya Sharma <ananyashar06@gmail.com>
2026-09-24 12:28:54 +0530

    Time: 9 ms (29.47%), Space: 46.6 MB (24.41%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length())
+        {
+            return false;
+        }
+        s=s.replaceAll("\\s","").toLowerCase();
+        t=t.replaceAll("\\s","").toLowerCase();
+        char[] arr1 = s.toCharArray();
+        char[] arr2 = t.toCharArray();
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        return Arrays.equals(arr1,arr2);
+        
+    }
+}
