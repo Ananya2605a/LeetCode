e928ea1fa19fe1f772d069b369f66911141d26ff
Ananya Sharma <ananyashar06@gmail.com>
2026-09-24 12:10:05 +0530

    Time: 6 ms (87.78%), Space: 44.1 MB (25.77%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        int[] mapS = new int[256];
+        int[] mapT = new int[256];
+
+        for(int i=0;i<s.length();i++)
+        {
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
+
+            if(mapS[c1]!=0 && mapS[c1]!=c2)
+            {
+                return false;
+            }
+
+            if(mapT[c2]!=0 && mapT[c2]!=c1)
+            {
+                return false;
+            }
+
+            mapS[c1]=c2;
+            mapT[c2]=c1;
+        }
+
+        return true;
+        
+    }
+}
